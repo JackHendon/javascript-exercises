ad221c792fd341de5d2e860e93376fd4a95033d6
Jack Hendon <hendonjk1@gmail.com>
2022-06-26 11:58:27 +0100

    Finish first 3 JS exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+result = "";
 
+const repeatString = function (string, num) {
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    result = string.repeat(num);
+  }
+  return result;
 };
+
+repeatString("hey");
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitString = str.split("");
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+    return joinArray
 };
 
+reverseString("Hello World")
 // Do not edit below this line
 module.exports = reverseString;
